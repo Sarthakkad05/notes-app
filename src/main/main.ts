7665d3f65be8f1ad64fb2d51b0732cd512e25bbd
Sarthakkad05 <kadsarthak33@gmail.com>
2026-03-21 20:25:05 +0530

    adding react-vite to setup

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,6 +1,8 @@
 import { app, BrowserWindow } from 'electron'
 import * as path from 'path'
 
+const isDev = process.env.NODE_ENV === 'development'
+
 function createWindow(): void {
   const win = new BrowserWindow({
     width: 800,
@@ -10,7 +12,12 @@ function createWindow(): void {
     }
   })
 
-  win.loadFile(path.join(__dirname, '../../index.html'))
+  if (isDev) {
+    win.loadURL('http://localhost:5173')
+    win.webContents.openDevTools()
+  } else {
+    win.loadFile(path.join(__dirname, '../../dist/renderer/index.html'))
+  }
 }
 
 app.whenReady().then(createWindow)
---
+++ src/renderer/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to Smart Notes</h1>
+    </div>
+  )
+}
+
+export default App
